700a847c329024b88b876e963b0b92dc3d4b8cb8
Adib-shadman <40189935+Adib-shadman@users.noreply.github.com>
2020-09-18 04:33:25 +1000

    First commit

---
+++ src/build/precache-manifest.8c84821d1792cd06808f77019dd150b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f6f43da642d150ee4df2de9882655fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5019e5e357a0d663647d",
+    "url": "/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "aee4eb3d9d76f9ddaf1a",
+    "url": "/static/js/2.b3fd54b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b3fd54b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5019e5e357a0d663647d",
+    "url": "/static/js/main.82f8c77a.chunk.js"
+  },
+  {
+    "revision": "d72424798ea2e40f89ab",
+    "url": "/static/js/runtime-main.8c1f49b6.js"
+  }
+]);
